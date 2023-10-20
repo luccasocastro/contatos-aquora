56ea6055a87d7ad72857f14f9c722744ddeb85b2
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-20 13:27:01 -0300

    feat: criando componente de contato e css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
