5c5fa1fd6e362e6d8b83fe16aed49a647ecd54f2
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-20 16:45:23 -0300

    fix: inserindo campo de imagem de perfil

--- backend/src/main/java/com/souzaluccas/backend/models/Contato.java
+++ backend/src/main/java/com/souzaluccas/backend/models/Contato.java
@@ -14,7 +14,8 @@ import jakarta.validation.constraints.NotNull;
 @Table(name = "TB_CONTATOS")
 public class Contato {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String nome;
@@ -24,46 +25,74 @@ public class Contato {
     private String telefone;
     @NotNull
     private LocalDate nascimento;
-    
+    @NotBlank
+    private String imagemPerfil;
+
     public Contato(Long id, @NotBlank String nome, @NotBlank String email, @NotBlank String telefone,
-            @NotNull LocalDate nascimento) {
+            @NotNull LocalDate nascimento, @NotBlank String imagemPerfil) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.nascimento = nascimento;
+        this.imagemPerfil = imagemPerfil;
     }
+
     public Contato() {
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getTelefone() {
         return telefone;
     }
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
     public LocalDate getNascimento() {
         return nascimento;
     }
+
     public void setNascimento(LocalDate nascimento) {
         this.nascimento = nascimento;
     }
-    
+
+    public String getImagemPerfil() {
+        return imagemPerfil;
+    }
+
+    public void setImagemPerfil(String imagemPerfil) {
+        this.imagemPerfil = imagemPerfil;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", nascimento="
+                + nascimento + ", imagemPerfil=" + imagemPerfil + "]";
+    }
+
 }
