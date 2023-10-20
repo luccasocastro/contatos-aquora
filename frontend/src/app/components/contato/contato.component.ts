a0bf25b14b018e19284688dbfeb7c1e20d12d051
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-20 16:44:26 -0300

    feat: página de contatos com dados da api

--- frontend/src/app/components/contato/contato.component.ts
+++ frontend/src/app/components/contato/contato.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Contato } from 'src/app/Contato';
+import { ContatoService } from './../../services/contato.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  styleUrls: ['./contato.component.css']
+  styleUrls: ['./contato.component.css'],
 })
-export class ContatoComponent {
+export class ContatoComponent implements OnInit{
+  constructor(private contatoService: ContatoService) {}
+
+  contatos: Contato[] = []
+
+  ngOnInit(): void {
+      this.contatoService.getAll().subscribe((contatos) => {
+        const data = contatos
+
+        data.map((contato) => {
+          contato.nascimento = new Date(contato.nascimento).toLocaleDateString(
+            'pt-BR'
+          )
+        })
+
+        this.contatos = contatos
+      })
+  }
 
 }
