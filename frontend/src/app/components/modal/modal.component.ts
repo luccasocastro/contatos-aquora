111900a0276f050ce411de05399db17337b3185c
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-22 23:08:58 -0300

    fix: consertando envio de imagens no client

--- frontend/src/app/components/modal/modal.component.ts
+++ frontend/src/app/components/modal/modal.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ModalService } from 'src/app/services/modal.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContatoService } from 'src/app/services/contato.service';
@@ -11,12 +10,12 @@ import { ContatoService } from 'src/app/services/contato.service';
 })
 export class ModalComponent implements OnInit {
   formContato!: FormGroup;
+  image?: File
 
   constructor(
     public modalService: ModalService,
     private formBuilder: FormBuilder,
-    private contatoService: ContatoService,
-    private router: Router
+    private contatoService: ContatoService
   ) {}
 
   ngOnInit(): void {
@@ -25,23 +24,35 @@ export class ModalComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       telefone: ['', Validators.required],
       nascimento: ['', Validators.required],
-      imagemPerfil: ['', Validators.required],
-    })
+      imagemPerfil: [null, Validators.required],
+    });
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.formContato.patchValue({ imagemPerfil: event.target.files[0] });
   }
 
   submit() {
-    if (this.formContato.valid) {
-      const dados = this.formContato.value
+    if(this.formContato.valid){
+      const formData = new FormData()
 
-      this.contatoService.createContato(dados).subscribe(
-        (resposta) => {
-          console.log('Dados enviados com sucesso!', resposta)
+      formData.append('nome', this.formContato.get('nome')!.value)
+      formData.append('email', this.formContato.get('email')!.value)
+      formData.append('telefone', this.formContato.get('telefone')!.value)
+      formData.append('nascimento', this.formContato.get('nascimento')!.value)
+      formData.append('imagemPerfil', this.formContato.get('imagemPerfil')!.value)
+
+      this.contatoService.criarContato(formData).subscribe(
+        (response) => {
+          console.log('Contato criado com sucesso!', response)
           this.formContato.reset()
           this.modalService.showModal = false
           window.location.href = '/';
         },
-        (erro) => {
-          console.error('Erro ao enviar os dados', erro);
+        (error) => {
+          console.log('Erro ao criar contato!', error)
         }
       )
     }
