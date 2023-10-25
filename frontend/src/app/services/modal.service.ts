93705cf7125b7f1e5b0b2d199d7a395af2724305
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-24 22:16:03 -0300

    feat: criando máscara para telefone

--- frontend/src/app/services/modal.service.ts
+++ frontend/src/app/services/modal.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { createMask } from '@ngneat/input-mask';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+
+  mask = '(99) 9 9999-9999'
+  public maskTelefone = createMask(this.mask)
 
   public showModalPost: boolean = false
   public showModalPut: boolean = false
